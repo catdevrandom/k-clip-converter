1e4cc60da0b3c3fb8be88e92e49d633fa5f5dfc5
Maira Carvalho <github@mairacarvalho.com>
2014-11-22 00:41:41 +0100

    Using argparser to receive arguments from command line. Reorganized the code dividing in classes. Added the outline of how the software should work

--- converter.py
+++ converter.py
@@ -1,34 +1,116 @@
 #!/usr/bin/python
 
 import re, sys, subprocess, os
+import argparse
 
-print 'Usage: convert.py <input> <output>'
+class mbp_reader():
+	def __init__(self, verbose):
+		self.path = os.path.dirname(os.path.realpath(__file__))
+		self.verbose = verbose
 
-input_file = str(sys.argv[1])
-output_file = str(sys.argv[2])
-#print input_file
+	def parsefile(self, inputfile):
+		#We need to go to the script's directory, as it calls other scripts in there
+		mbppath = self.path + os.sep + 'lib' + os.sep + 'MBP_reader'
+		os.chdir(mbppath)
+		sp = subprocess.Popen(["perl", 'MBP_reader.pl' , inputfile],  stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+		result, errors = sp.communicate()
+		#Then we can move back to the original dir
+		os.chdir(self.path)
+		if self.verbose:
+			print ''
+			print '-----------------------------------------------------'
+			print '----------- OUTPUT of MBP_Reader --------------------'
+			print result
+			print ''
+			print '                   --- &&& ---                       '
+			print 'FURTHER INFORMATION: '
+			print errors
+			print '-----------------------------------------------------'
+			print ''
+		output_contents = self.readfile(inputfile)
+		return output_contents
+		
+	def readfile(self, inputfile):
+		outputfile = inputfile + '_notes.txt'
+		if self.verbose:
+			print 'The output file was generated successfully: "%s"' % outputfile
+		f = open(outputfile)
+		filecontents = f.read()
+		f.close()
+		if self.verbose:
+			print 'Removing the output file...'
+		try:
+			os.remove(outputfile)
+			if self.verbose:
+				print 'File removed successfully.'
+		except:
+			e = sys.exc_info()[0]
+			print "Error: %s" % e
+		return filecontents
+		
 
-my_path = os.path.dirname(os.path.realpath(__file__))
+class formatter():
+	def __init__(self, verbose):
+		self.path = os.path.dirname(os.path.realpath(__file__))
+		self.verbose = verbose
+		
+	def convert(self, inputstream):
+	
+		# Split the input in several notes
+		notes = inputstream.split("\n--\n")
+		
+		# Iterate over each note
+		
+		# Inside the loop, call the function parse note
+		
+		# The result is a note split in parts
+		
+		# format each single note
+		
+		# Build a new list with formated notes
+		
+		# Write the formatted note to a (temp) file, note by note
+		
+		# return the temp file		
+		return notes
 
-os.chdir('MBP_Reader') #Go down to the module directory to run the PERL script
-pipe = subprocess.Popen(["perl", "MBP_reader.pl", input_file],  stdout=subprocess.PIPE)
-result = pipe.stdout.read()
-os.chdir('..') #Go back to the Python script dir
-#print result
-output_txt = input_file + '_notes.txt'
+	def parsenote(self, note):
+		#Perform regex
+		if self.verbose:
+			print "Reading the input to split the single note in its parts"
+			
+	def formatsinglenote(self, tornnote):
+		#This function takes a dictionary with the note parts and builds one note
+		pass
 
-f = open(output_txt)
-file_contents = f.read()
-f.close()
 
-converted_contents = ''
-#Regex substitution starts
+def main():
+	argparser = argparse.ArgumentParser(description='Gets an MBP input and converts it to a valid "My Clippings.txt" file.')
+	argparser.add_argument('inputfile', help='The file to convert. Inform with the full path.')
+	argparser.add_argument('-o', '--output', help='The output file. If not informed, outputs to stdin.')
+	argparser.add_argument('-v', '--verbose', action='store_const', const=1, default=0, help='Use this flag to get all the output to the console')
+	args = argparser.parse_args()
 
-notes = file_contents.split("\n--\n")
-print notes
+	inputfile = args.inputfile
+	output = args.output
+	verbose = args.verbose
+    
+	# Run the MBP_parser
+	myMbp = mbp_reader(verbose)
+	intermediateOutput = myMbp.parsefile(inputfile)
 
-#Regex ends
+	# Run the formatter to convert the MBP parser results into a valid My Clippings file
+	myFormatter = formatter(verbose)
+	results = myFormatter.convert(intermediateOutput)
+	
+	# If the user specified an output file, this is the time to print it. otherwise, return it to STDOUT
+	
+	if output:
+		print 'writing output to file %s' % output
+	else:
+		pass #print results
 
-f2 = open(output_file,'w')
-f2.write(converted_contents)
-f2.close()
+
+if  __name__=="__main__":
+	main();
+	
